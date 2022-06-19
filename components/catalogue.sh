da71f49070e57a332129f779b59c4d1904d3b016
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-18 20:16:20 -0700

    updating ./roboshop-shell-script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,1 +1,18 @@
-echo install catalogue component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+
+
+unzip -o /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl restart catalogue
+systemctl enable catalogue
--- components/mongodb.sh
+++ components/mongodb.sh
@@ -3,10 +3,13 @@ yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
 systemctl restart mongod
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
-unzip mongodb.zip
+unzip -o mongodb.zip
 cd mongodb-main
 mongo < catalogue.js
 mongo < users.js
