b0ba49aba1fa6d0516ff0c383473b90fda96b69a
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-18 20:57:56 -0700

    updating ./roboshop-shell-script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -11,6 +11,7 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
