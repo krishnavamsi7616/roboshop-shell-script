a02cd29d2258d6099fdab73c3a90539d4a22aa98
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-19 14:28:56 -0700

    updating ./roboshop-shell-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -5,16 +5,3 @@ systemctl start mysqld
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-
-
-'''
-grep temp /var/log/mysqld.log
-mysql_secure_installation
-mysql -uroot -pRoboShop@1
-
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-
-cd /tmp
-unzip mysql.zip
-cd mysql-main
-mysql -u root -pRoboShop@1 < shipping.sql
